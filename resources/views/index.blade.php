559ed88102aee8c6d8bf62498bef245d44f10ba9
GameGodS3 <sudevssuresh@gmail.com>
2022-11-30 21:33:47 +0530

    Added: CSS, fonts, images, index page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="/css/styles.css" />
+    <title>GreenPath</title>
+  </head>
+  <body>
+    <nav>
+      <div class="logo">
+        <img
+          src="/images/greenpath-logo-long.svg"
+          alt=""
+          class="logo-img"
+        />
+      </div>
+      <div class="nav-menu">
+        <ul class="nav-links">
+          <a href="">
+            <li class="nav-item">Home</li>
+          </a>
+          <a href="">
+            <li class="nav-item">About</li>
+          </a>
+          <a href="">
+            <li class="nav-item">Contact Us</li>
+          </a>
+          <a href="">
+            <li class="nav-item cta-secondary">Sign Up</li>
+          </a>
+          <a href="">
+            <li class="nav-item cta-sec">Log In</li>
+          </a>
+        </ul>
+      </div>
+    </nav>
+    <main>
+      <section class="landing-hero">
+        <div class="hero-left">
+          <h1 class="hero-heading">Lorem ipsum dolor sit amet consectetur.</h1>
+          <h2 class="hero-subheading">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae,
+            atque!
+          </h2>
+          <div class="cta">Get Started</div>
+        </div>
+        <div class="hero-right">
+          <img src="/images/hero2.svg" alt="" />
+        </div>
+      </section>
+      <section></section>
+      <section></section>
+    </main>
+    <footer></footer>
+  </body>
+</html>
